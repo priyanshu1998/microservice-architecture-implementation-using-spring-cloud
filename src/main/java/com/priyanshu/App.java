3e974a1dca3455ebb78d7915adcd4923772adc4f
Priyanshu Tirkey <priyanshutirkey1998@gmail.com>
2022-07-25 18:03:30 +0530

    initial commit

---
+++ src/main/java/com/priyanshu/App.java
@@ -0,0 +1,13 @@
+package com.priyanshu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

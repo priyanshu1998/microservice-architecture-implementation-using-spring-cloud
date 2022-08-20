a5543d72ee5bc04a8fa1df86dafe4b0b52e44bde
Priyanshu Tirkey <priyanshutirkey1998@gmail.com>
2022-08-20 20:24:50 +0530

    implemented Fraud controller, along with Fraud and Customer microservice communication

---
+++ customer/src/main/java/com/priyanshu/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.priyanshu.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/priyanshu/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.priyanshu.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
